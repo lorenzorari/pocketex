0aaa1f122ac36721b167527bf3393cf86bc818c1
Lorenzo Ferrari <lorenzofg24@gmail.com>
2025-02-24 23:41:16 +0100

    Stats: Display total row to avoid flickering

--- src/features/pokemon-details/components/stats/Stats.tsx
+++ src/features/pokemon-details/components/stats/Stats.tsx
@@ -17,11 +17,7 @@ export function Stats({ stats }: Props) {
         {statTabs.map((tab) => (
           <Button
             key={tab.value}
-            variant={
-              activeTab === tab.value
-                ? ButtonVariant.Dark
-                : ButtonVariant.Outline
-            }
+            variant={activeTab === tab.value ? ButtonVariant.Dark : ButtonVariant.Outline}
             onClick={tab.onClick}
           >
             {tab.label}
@@ -30,21 +26,20 @@ export function Stats({ stats }: Props) {
       </div>
 
       {statFields.map(({ label, range, rangeLimit }) => (
-        <StatField
-          key={label}
-          label={label}
-          range={range}
-          rangeLimit={rangeLimit}
-        />
+        <StatField key={label} label={label} range={range} rangeLimit={rangeLimit} />
       ))}
-      {statFields[0].range.length === 1 && (
-        <PanelField className="flex justify-end gap-3">
-          <PanelLabel>Total</PanelLabel>
-          <div className="w-[34px] text-right">
-            <span className="font-bold">{calculateStatSum()}</span>
-          </div>
-        </PanelField>
-      )}
+      <PanelField className="flex justify-end gap-3">
+        {statFields[0].range.length === 1 ? (
+          <>
+            <PanelLabel>Total</PanelLabel>
+            <div className="w-[34px] text-right">
+              <span className="font-bold">{calculateStatSum()}</span>
+            </div>
+          </>
+        ) : (
+          <span className="invisible">0</span>
+        )}
+      </PanelField>
     </Panel>
   );
 }
