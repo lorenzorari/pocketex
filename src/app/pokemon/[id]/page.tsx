4bff719dade293ed6972bfa9f3926491ab6f4bcb
Lorenzo Ferrari <lorenzofg24@gmail.com>
2025-03-22 19:52:42 +0100

    Move description function to Species service

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -21,44 +21,17 @@ interface Props {
 const DetailsPage = async ({ params }: Props) => {
   const pokemonId = (await params).id;
   const pokemon = await getPokemon(pokemonId);
-  const { species, genus } = await getSpecies(pokemonId);
+  const { species, genus, description } = await getSpecies(pokemonId);
 
   const pokemonTypes = (pokemon?.types?.map(({ type }) => type.name) || []) as PokemonType[];
   const typeEffectiveness = await getTypeEffectiveness(pokemonTypes);
   const logoColorCSS = `var(--color-${pokemonTypes?.[0]}-1)`;
 
-  // useEffect(() => {
-  //   if (pokemon?.name) {
-  //     document.title = `${pokemon.name} | Pocketex`;
-  //   }
-  // }, [pokemon]);
-
-  const getDescription = () => {
-    const text = species?.flavorTextEntries?.find(({ language }) => language.name === 'en')?.flavorText;
-
-    if (!text) return '';
-
-    return text
-      ?.replace(/u'\f'/, ' ')
-      .replace(/\u00AD/g, '')
-
-      .replace(/\u000C/g, ' ')
-      .replace(/u' -\n'/, ' - ')
-      .replace(/u'-\n'/, '-')
-      .replace(/(\r\n|\n|\r)/gm, ' ');
-  };
-
   return (
-    // <PrimaryTypeCtx value={pokemonTypes?.[0] as PokemonType}>
     <DefaultLayout logoColorCSS={logoColorCSS}>
       {pokemon ? (
         <>
-          <PokemonDetailHero
-            genus={genus}
-            pokemon={pokemon}
-            pokemonTypes={pokemonTypes}
-            description={getDescription()}
-          />
+          <PokemonDetailHero genus={genus} pokemon={pokemon} pokemonTypes={pokemonTypes} description={description} />
           {species && (
             <div className="relative z-10 rounded-t-[40px] bg-white px-5 py-10 shadow-[0px_100px_484px_0px_rgba(0,0,0,0.4)] lg:-mt-16 lg:px-10 lg:py-[60px] xl:px-32">
               <div className="mb-10 grid gap-10 md:grid-cols-[repeat(auto-fit,minmax(340px,1fr))]">
@@ -73,7 +46,6 @@ const DetailsPage = async ({ params }: Props) => {
         </>
       ) : null}
     </DefaultLayout>
-    // {/* </PrimaryTypeCtx> */}
   );
 };
 
--- src/services/species.ts
+++ src/services/species.ts
@@ -5,6 +5,7 @@ import { type Species } from '@/models/species';
 type GetSpeciesData = {
   species: Species;
   genus: string;
+  description: string;
 };
 
 const BASE_URL = 'pokemon-species';
@@ -12,8 +13,9 @@ const BASE_URL = 'pokemon-species';
 export async function getSpecies(pokemonId: string): Promise<GetSpeciesData> {
   const species = await api<Species>(`${BASE_URL}/${pokemonId}`);
   const genus = getGenus(species);
+  const description = getDescription(species);
 
-  return { species, genus };
+  return { species, genus, description };
 }
 
 function getGenus(species: Species) {
@@ -23,6 +25,21 @@ function getGenus(species: Species) {
 
   return genus.replace('Pokémon', '').trim();
 }
+
+const getDescription = (species: Species) => {
+  const text = species?.flavorTextEntries?.find(({ language }) => language.name === 'en')?.flavorText;
+
+  if (!text) return '';
+
+  return text
+    ?.replace(/u'\f'/, ' ')
+    .replace(/\u00AD/g, '')
+
+    .replace(/\u000C/g, ' ')
+    .replace(/u' -\n'/, ' - ')
+    .replace(/u'-\n'/, '-')
+    .replace(/(\r\n|\n|\r)/gm, ' ');
+};
 
 export async function getSpeciesPagination(offset: number = 0, limit: number = 20) {
   const params = offset || limit ? `?offset=${offset}&limit=${limit}` : '';
