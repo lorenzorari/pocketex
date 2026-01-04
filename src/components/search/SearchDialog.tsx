442f100d5f4fa5f9cd0a8e044ebf502cdd79b2d7
Lorenzo Ferrari <lorenzofg24@gmail.com>
2026-01-04 21:16:34 +0100

    Fix and lock search dialog overlay

--- src/components/search/SearchDialog.tsx
+++ src/components/search/SearchDialog.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useEventListener, useScrollLock } from 'usehooks-ts';
 import ClientOnlyPortal from '@/components/ClientOnlyPortal';
 import { CommandDialog } from '@/components/Command';
+import { Overlay } from '@/components/dialog/Overlay';
 import Search from '@/components/search/Search';
 import { cn } from '@/utils/classnames';
 
@@ -14,27 +15,20 @@ interface Props {
 export default function SearchDialog({ children }: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { lock, unlock, isLocked } = useScrollLock({ autoLock: false });
+  const { lock, unlock } = useScrollLock({ autoLock: false });
 
   useEventListener('keydown', (e) => {
     if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
       e.preventDefault();
-      const newIsOpen = !isOpen;
-
-      if (newIsOpen) lock();
-      else unlock();
-
-      setIsOpen(newIsOpen);
+      handleOpenChange(!isOpen);
     }
   });
 
-  function handleOpenChange(value: boolean) {
-    const newIsOpen = !value;
+  function handleOpenChange(newValue: boolean) {
+    const lockAction = newValue ? lock : unlock;
 
-    if (newIsOpen) lock();
-    else unlock();
-
-    setIsOpen(newIsOpen);
+    lockAction();
+    setIsOpen(newValue);
   }
 
   return (
@@ -42,15 +36,11 @@ export default function SearchDialog({ children }: Props) {
       {children({ isOpen, setIsOpen })}
 
       <ClientOnlyPortal selector="body">
-        <div
-          className={cn('absolute inset-0 opacity-0 backdrop-blur-xl transition-opacity duration-200', {
-            'z-50 opacity-100': isOpen,
-          })}
-        >
+        <Overlay className={cn('opacity-0', { 'z-50 opacity-100': isOpen })}>
           <CommandDialog open={isOpen} onOpenChange={handleOpenChange}>
             <Search />
           </CommandDialog>
-        </div>
+        </Overlay>
       </ClientOnlyPortal>
     </>
   );
