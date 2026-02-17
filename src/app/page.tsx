a3d8b6f06f742a6edeea3514bb3b3d5ee9f438a8
Lorenzo Ferrari <lorenzofg24@gmail.com>
2026-02-17 18:15:46 +0100

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { type Viewport } from 'next';
 import { HeroBanner } from '@/components/hero-banner';
-import { COLOR_PRIMARY } from '@/constants';
 import { HomeLayout } from '@/layouts/HomeLayout';
 
-export const viewport: Viewport = {
-  themeColor: COLOR_PRIMARY,
-};
+// export const viewport: Viewport = {
+//   themeColor: COLOR_PRIMARY,
+// };
 
 export default function HomePage() {
   return (
