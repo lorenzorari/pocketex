557235dd642902c55ba370f9c5e50f59d440bedf
Lorenzo Ferrari <lorenzofg24@gmail.com>
2026-03-01 20:04:12 +0100

    SEO WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Pocketex',
   description: 'A mini but cool Pokédex',
   icons: '/assets/svg/logo.svg',
+  verification: {
+    google: 'DSgL2xeRX1u2zjkIo9TG9DCsbDVd7mtMmVbOOtgP6Yg',
+  },
 };
 
 export default function RootLayout({
